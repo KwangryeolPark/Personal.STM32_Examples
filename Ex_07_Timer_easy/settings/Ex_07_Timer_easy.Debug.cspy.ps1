ce118df7109b0a660355752a74f82e229423294e
KwangryeolPark <pkr7098@gmail.com>
2021-02-12 00:39:24 +0900

    정규 업데이트

--- Ex_07_Timer_easy/settings/Ex_07_Timer_easy.Debug.cspy.ps1
+++ Ex_07_Timer_easy/settings/Ex_07_Timer_easy.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "E:\ARM\Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.general.xcl" --backend -f "E:\ARM\Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "E:\ARM\STM32_Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.general.xcl" --backend -f "E:\ARM\STM32_Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "E:\ARM\Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\ARM\Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.driver.xcl" 
+& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.4\common\bin\cspybat" -f "E:\ARM\STM32_Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\ARM\STM32_Examples\Ex_07_Timer_easy\settings\Ex_07_Timer_easy.Debug.driver.xcl" 
 }
